7861b22abd55c42f9e27b0772800365de294713a
xwatson <xwatson@foxmail.com>
2019-04-11 19:12:40 +0800

    update tabs config

---
+++ src/config/tabs.js
@@ -0,0 +1,30 @@
+
+const Tabs = [
+  {
+    path: '/home',
+    title: '首页',
+    defaultSelected: true,
+    icon: 'icon-home2',
+    selectedIcon: 'icon-home_fill',
+  },
+  {
+    path: '/categories',
+    title: '分类',
+    icon: 'icon-category',
+    selectedIcon: 'icon-category_fill',
+  },
+  {
+    path: '/cart',
+    title: '购物车',
+    icon: 'icon-cart',
+    selectedIcon: 'icon-cart_fill',
+  },
+  {
+    path: '/my',
+    title: '我的',
+    icon: 'icon-me',
+    selectedIcon: 'icon-me_fill',
+  },
+]
+
+export default Tabs
