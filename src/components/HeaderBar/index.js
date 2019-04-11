883f26198dc088d60d79644031a95a620e465313
xwatson <xwatson@foxmail.com>
2019-04-11 17:39:56 +0800

    优化组件

--- src/components/HeaderBar/index.js
+++ src/components/HeaderBar/index.js
@@ -1,36 +1,31 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { NavBar, Icon } from 'antd-mobile'
 
-export default class HeaderBar extends React.PureComponent {
-  static defaultProps = {
-    mode: 'light',
-    title: '',
-  }
-  static propTypes = {
-    history: PropTypes.object.isRequired,
-    options: PropTypes.object
-  }
-
-  render() {
-    const { history, options = {} } = this.props
-    const pathLength = history.location.pathname.split('/').length
-    const defaultProps = { ...this.props, ...options }
-    if (pathLength > 2) {
-      defaultProps.icon = <Icon type="left" />
-      defaultProps.onLeftClick = (e) => {
-        let isPreventDefault = true
-        if (typeof options.onChange === 'function') {
-          isPreventDefault = options.onChange(e, 'onLeftClick') // 参数：event，事件类型
-        }
-        if (isPreventDefault !== false) {
-          history.goBack()
-        }
+const HeaderBar = (props) => {
+  const { history, options = {} } = props
+  const pathLength = history.location.pathname.split('/').length
+  const defaultProps = { ...props, ...options }
+  if (pathLength > 2) {
+    defaultProps.icon = <Icon type="left" />
+    defaultProps.onLeftClick = (e) => {
+      let isPreventDefault = true
+      if (typeof options.onChange === 'function') {
+        isPreventDefault = options.onChange(e, 'onLeftClick') // 参数：event，事件类型
+      }
+      if (isPreventDefault !== false) {
+        history.goBack()
       }
     }
-    delete defaultProps.options
-    return (
-      <NavBar {...defaultProps} >{defaultProps.title}</NavBar>
-    )
   }
+  delete defaultProps.options
+  return (
+    <NavBar {...defaultProps} >{defaultProps.title}</NavBar>
+  )
 }
+
+HeaderBar.defaultProps = {
+  mode: 'light',
+  title: '',
+}
+
+export default HeaderBar
