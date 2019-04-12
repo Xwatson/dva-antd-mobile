b2bc4784051e8d73380155ea5e7a7e01595d513d
xwatson <xwatson@foxmail.com>
2019-04-12 16:12:53 +0800

    add alias

--- .webpackrc.js
+++ .webpackrc.js
@@ -7,6 +7,9 @@ export default {
   ],
   theme: './theme.config.js',
   alias: {
+    '@components': path.resolve(__dirname, './src/components'),
+    '@config': path.resolve(__dirname, './src/config'),
+    '@routes': path.resolve(__dirname, './src/routes'),
     '@page': path.resolve(__dirname, './src/components/Page/index.js'),
   },
   html: {
